55238d70b9c4a643ea3b51457406fccf475432c2
devignasrocys <devignasrocys@gmail.com>
2023-02-22 09:41:42 +0200

    update

--- ptu8_library/library/admin.py
+++ ptu8_library/library/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from . import models
 
+class BookAdmin(admin.ModelAdmin):
+    list_display = ("author", "title")
+
 # Register your models here.
 admin.site.register(models.Genre)
 admin.site.register(models.Author)
-admin.site.register(models.Book)
+admin.site.register(models.Book, BookAdmin)
 admin.site.register(models.BookInstance)
